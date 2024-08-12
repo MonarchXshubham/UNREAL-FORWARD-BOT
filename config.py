7c202005f7c69e70d14d8810d17210128a51b27a
nothing... <161198246+PYHOMES00@users.noreply.github.com>
2024-08-12 14:29:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://smkbotz:YPYAFx0wxDqTiyfE@cluster0.auq84bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002235766670")
+    LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002235766670")
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002235661888") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
