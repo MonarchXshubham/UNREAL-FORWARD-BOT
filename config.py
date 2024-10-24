f620bd22985cf1cd2acda8c3d2981f3c680f1621
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-10-25 01:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ class Config:
     
     API_ID = environ.get("API_ID", "28243586")
     API_HASH = environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7273354975:AAGrkcgUdrdUvitwdger1XfA8vtHcmLGIuE") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5340652544').split()]
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7064899225:AAF80y8D2VbmKb8S_k5PgKBq5hjRV5L3Rcs") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6929340267 6551906246').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
-    PICS = (environ.get('PICS', 'https://telegra.ph/file/a87e65f65e37aafd745df.jpg'))
+    PICS = (environ.get('PICS', 'https://vault.pictures/p/4add4cab289149138c7f5ea4ec9d4813'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://smkbotz:YPYAFx0wxDqTiyfE@cluster0.auq84bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "DATABASE_URI", "mongodb+srv://madarazbotz:MJlmnDPtC63ihNK5@cluster0.7dxhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002235766670"))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002303065410"))
     
-    UPDATES_CHANNEL = "-1002235661888"
+    UPDATES_CHANNEL = "-1002197951128"
 
 
 class temp(object): 
